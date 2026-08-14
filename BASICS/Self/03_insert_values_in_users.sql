e080b0c2800c0beea16943735a805bfdb16b2de2
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-08-14 15:44:11 +0530

    query for inserting values to users table added

---
+++ BASICS/Self/03_insert_values_in_users.sql
@@ -0,0 +1,28 @@
+-- Insert values in table 'users'
+
+
+INSERT INTO users (id, name, email, gender, date_of_birth, salary) VALUES
+(1, 'Aarav', 'aarav@example.com', 'Male', '1995-05-14', 65000),
+(2, 'Ananya', 'ananya@example.com', 'Female', '1990-11-23', 72000),
+(3, 'Raj', 'raj@example.com', 'Male', '1988-02-17', 58000),
+(4, 'Sneha', 'sneha@example.com', 'Female', '2000-08-09', 50000),
+(5, 'Farhan', 'farhan@example.com', 'Male', '1993-12-30', 61000),
+(6, 'Priyanka', 'priyanka@example.com', 'Female', '1985-07-12', 84000),
+(7, 'Aisha', 'aisha@example.com', 'Female', '1997-03-25', 56000),
+(8, 'Aditya', 'aditya@example.com', 'Male', '1992-06-17', 69000),
+(9, 'Meera', 'meera@example.com', 'Female', '1989-09-05', 77000),
+(10, 'Ishaan', 'ishaan@example.com', 'Male', '2001-10-02', 45000),
+(11, 'Tanvi', 'tanvi@example.com', 'Female', '1994-04-18', 62000),
+(12, 'Rohan', 'rohan@example.com', 'Male', '1986-12-01', 75000),
+(13, 'Zoya', 'zoya@example.com', 'Female', '1998-01-15', 54000),
+(14, 'Karan', 'karan@example.com', 'Male', '1990-08-22', 68000),
+(15, 'Nikita', 'nikita@example.com', 'Female', '1987-03-10', 71000),
+(16, 'Manav', 'manav@example.com', 'Male', '1996-11-29', 61000),
+(17, 'Divya', 'divya@example.com', 'Female', '1991-02-28', 57000),
+(18, 'Harshit', 'harshit@example.com', 'Male', '1993-09-09', 65000),
+(19, 'Ritika', 'ritika@example.com', 'Female', '1999-05-05', 52000),
+(20, 'Imran', 'imran@example.com', 'Male', '1995-07-30', 63000),
+(21, 'Juhi', 'juhi@example.com', 'Female', '1992-10-14', 59000),
+(23, 'Lata', 'lata@example.com', 'Female', '1984-11-11', 78000),
+(24, 'Yash', 'yash@example.com', 'Male', '1997-06-06', 64000),
+(25, 'Fatima', 'fatima@example.com', 'Female', '1993-03-03', 50533);
