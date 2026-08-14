dc4438dd43720318649dd008e868b34152af2354
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-08-14 15:30:05 +0530

    added users table creation sql code

---
+++ BASICS/Self/01_Create_table_users.sql
@@ -0,0 +1,15 @@
+-- Create a table name it as users 
+-- id(auto-increment), name, email, gender, date_of_birth, salary
+
+CREATE DATABASE IF NOT EXISTS startersql;
+USE startersql;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    gender ENUM('Male', 'Female', 'Other'),
+    date_of_birth DATE,
+    salary DECIMAL(10, 2),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
