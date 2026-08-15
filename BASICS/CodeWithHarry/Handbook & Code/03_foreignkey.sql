c6a8b017e1e38072505784f55b77a0c69dd20577
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-08-15 14:54:39 +0530

    added updates

--- BASICS/CodeWithHarry/Handbook & Code/03_foreignkey.sql
+++ BASICS/CodeWithHarry/Handbook & Code/03_foreignkey.sql
@@ -1,15 +1,4 @@
-USE startersql;
-DROP TABLE IF EXISTS addresses;
-
-CREATE TABLE addresses (
-id INT AUTO_INCREMENT PRIMARY KEY,
-user_id INT,
-street VARCHAR(255),
-city VARCHAR(100),
-state VARCHAR(100),
-pincode VARCHAR(10),
-CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
+-- insert values in address table
 
 INSERT INTO addresses (user_id, street, city, state, pincode)
 VALUES
