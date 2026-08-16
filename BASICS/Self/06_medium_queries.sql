91de391777b27bdfc70c55b4267f545dc7bf4ad9
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-08-16 18:42:19 +0530

    updated with 2 new questions

--- BASICS/Self/06_medium_queries.sql
+++ BASICS/Self/06_medium_queries.sql
@@ -18,4 +18,30 @@ from users
 inner join addresses on users.id=addresses.user_id
 where users.salary between 60000 and 750000;
 
+-- Display all addresses and the corresponding user's name using a RIGHT JOIN.
+select addresses.street,
+       addresses.city,
+       addresses.state,
+       addresses.pincode,
+       users.name
+from users
+right join addresses on users.id=addresses.user_id;
+
+-- Display all users and their states using a LEFT JOIN.
+select users.name , addresses.state
+from users
+left join addresses on users.id=addresses.user_id;
+
+-- Using INNER JOIN, display users who have an address in Delhi.
+select users.name
+from users
+inner join addresses on users.id=addresses.user_id
+where addresses.city='Delhi';
+
+-- Using LEFT JOIN, display every user's name and their address pincode.
+select users.name ,addresses.pincode
+from users
+left join addresses on users.id=addresses.user_id
+
+
 
